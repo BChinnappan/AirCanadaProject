72c192849123d57c79f7120ae592923261605449
BChinnappan <barnabasc@gmail.com>
2020-05-18 16:07:05 +0530

    Aircanda code was updated

--- src/test/java/Org/TCS/AircanadaStructure.java
+++ src/test/java/Org/TCS/AircanadaStructure.java
@@ -4,6 +4,9 @@ public class AircanadaStructure {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Welcome to AirCanada Project");
+		System.out.println("How to clone the project in GitHub");
+		
 
 	}
 
